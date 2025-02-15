e8daf3401bc12ed925eac5ff20d4266dec50293a
Kay <superkay0101@gmail.com>
2025-02-15 15:27:24 +0800

    完成

--- 2025-test/src/App.jsx
+++ 2025-test/src/App.jsx
@@ -1,43 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import axios from 'axios';
+
 
 function App() {
 
-  console.log("tttt");
+  const [users, setUsers] = useState([]);
 
-  const [users, setUsers] = useState(["123"]);
-
-  console.log(users);
-  
-  // axios.get('https://randomuser.me/api/?results=10')
-  // .then((res)=>{
-  //   setUsers(res.data.results);
-  //   console.log(users);
-  // })
-  // .catch((error)=>{
-  //   console.log(error);
-  // })
+  useEffect(()=>{
+    (async()=>{
+      try {
+        const res = await axios.get('https://randomuser.me/api/?results=10');
+        setUsers(res.data.results);
+        // console.log(res.data.results);
+        console.log(users);
+      } catch (error) {
+        console.log(error);
+      }
+    })();  
+  },[]);
 
   return (
-    <div className="container mx-auto p-4">
-      <div className="row">
-        {/* {
-          userData.map((user,index)=>{
+    <>
+      <div className="container mx-auto p-4">
+      <div className="row g-3">
+        {
+          users.map((user,index)=>{
             return (
               <div className="col-md-4" key={index}>
-                <div className="bg-light p-3">
+                <div className="bg-light p-3 text-center">
                   <img
-                    src="https://randomuser.me/api/portraits/women/61.jpg"
+                    src={user.picture.large}
                     alt="頭像"
                     className="img-fluid rounded-circle"
                   />
-                  <h2 className="mb-0">Mona Heen</h2>
-                  <p className="mb-0">mona.heen@example.com</p>
+                  <h2 className="mb-0">{user.name.last} {user.name.first}</h2>
+                  <p className="mb-0">{user.email}</p>
                 </div>
               </div>
             )
           })
-        } */}
-        <div className="col-md-4">
+        }
+        {/* <div className="col-md-4">
           <div className="bg-light p-3">
             <img
               src="https://randomuser.me/api/portraits/women/61.jpg"
@@ -58,10 +63,11 @@ function App() {
             <h2 className="mb-0">Susan Craig</h2>
             <p className="mb-0">susan.craig@example.com</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
-  );
+    </>
+  )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+export default App
